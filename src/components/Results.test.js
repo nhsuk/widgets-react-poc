e99c9bc3d5c780e2b0b7cc787721a000c01ddfb2
Tom Doughty <thomashdoughty@gmail.com>
2021-06-18 13:55:49 +0100

    Fix results test

--- src/components/Results.test.js
+++ src/components/Results.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Results from './Results';
 
 test('renders learn react link', () => {
-  render(<Results />);
+  render(<Results organisations={[]} />);
   const linkElement = screen.getByText(/Go back/i);
   expect(linkElement).toBeInTheDocument();
 });
